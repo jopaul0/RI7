0021788c954c222c1d40117bf948422b3ea87ba1
João Paulo <jopaulo.as8@gmail.com>
2026-03-04 22:13:29 -0300

    chore: implementando uma nova interface

--- src/core/Operacao.ts
+++ src/core/Operacao.ts
@@ -1,3 +1,5 @@
-export default abstract class Operacao {
+import { ICalculavel } from "../interface/ICalculo";
+
+export default abstract class Operacao implements ICalculavel {
     public abstract calcular(a: number, b: number, c:number): number | number[];
 }
